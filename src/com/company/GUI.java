1d11f3d92efdd8403e642333ffec8ff7e9353a32
EwanLorimer <ewan.lorimer@outlook.com>
2022-10-14 18:01:22 +0100

    GUI and retriever created

---
+++ src/com/company/GUI.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class GUI {
+    //Use Swing to create a front end for the user
+
+    public GUI(){
+        //Constructor to be used
+    }
+}
---
+++ src/com/company/MarketInfoRetriever.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class MarketInfoRetriever {
+    //User Yahoo FinanceAPI to retrieve real time market data
+    public MarketInfoRetriever(){
+        //Constructor
+    }
+}
