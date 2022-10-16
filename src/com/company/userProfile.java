4def4a47369c6fb08b4738e34e95301025a82ad3
EwanLorimer <ewan.lorimer@outlook.com>
2022-10-16 02:08:55 +0100

    userProfile ammended

--- src/com/company/userProfile.java
+++ src/com/company/userProfile.java
@@ -1,6 +1,24 @@
 package com.company;
-public class userProfile {
-    //Contains all the info for the user
-    //Contents will need to be saved into a CSV file to keep the users profile
+import java.io.File; //Allows us to parse text file
+import java.io.IOException; //IO Exception for handling file errors
+
+public class userProfile{
+    //Contains all the info for the user about the stocks they like
+    //This can be kept in a text file
+
+
+    public userProfile(){
+
+    }
+
+    //Create the file if it doesn't already exist
+    public void createFile(){
+        
+    }
+
+
+
+
+
 
 }
