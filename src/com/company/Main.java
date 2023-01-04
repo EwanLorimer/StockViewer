47538bd4151a4ec676c0004acebc363dbc0d4eaa
EwanLorimer <ewan.lorimer@outlook.com>
2023-01-04 15:41:58 +0000

    Test Message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args){
         MarketPage page = new MarketPage();
+        System.out.println("Running; Test Successful");
         }
 
 }
